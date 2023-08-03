b7f511f775bbedb6b077a990a1b671342fc2fb5c
hectom <118475332+hectom@users.noreply.github.com>
2023-08-03 14:48:11 -0300

    enviei o arquivo

---
+++ agendasimplesmeupaiélindo/Contato.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace agenda_simples_t5
+{
+    internal class Contato
+    {
+        // Variáveis locais da classe Contato.
+        // Acessíveis apenas pela classe Contato.
+        private string primeiroNome;
+        private string sobrenome;
+        private string telefone;
+        private string email;
+        private string idade;
+
+        // PROPRIEDADES da classe Contato.
+        // Acessíveis por qualquer parte desse programa.
+        public string PrimeiroNome
+        {
+            get { return primeiroNome; }
+            set { primeiroNome = value; }
+        }
+        public string Sobrenome
+        {
+            get { return sobrenome; }
+            set { sobrenome = value; }
+        }
+        public string Telefone
+        {
+            get { return telefone; }
+            set 
+            { 
+                if (value.Length == 11)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "00-00000-0000";
+                }
+            }
+        }
+         public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Idade
+        {
+            get { return idade; }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    idade = value;
+                }
+                else
+                {
+                    idade = "000";
+                }
+
+            }
+        }
+        
+        // Construtor da classe.
+        public Contato()
+        {
+            PrimeiroNome = "João";
+            Sobrenome = "Da Silva";
+            Telefone = "11988888776";
+            Idade = "18";
+            Email = "joaodasilva@gmail.com";
+
+        }
+        // Sobrecarga de método.
+        public Contato(string primeiroNome, string sobrenome, string telefone, string idade, string email)
+        {
+            PrimeiroNome = primeiroNome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+            Idade = idade;
+            Email = email;
+        }
+
+        // Sobreescrita do método ToString().
+        public override string ToString()
+        {
+            // Melhor utilizar String.Empty invés de "".
+            string saida = String.Empty;
+            saida += String.Format("{0} {1}", PrimeiroNome, Sobrenome);
+            saida+= String.Format()
+            saida += String.Format("{0}-{1}-{2}", 
+                Telefone.Substring(0,2),
+                Telefone.Substring(2,5),
+                Telefone.Substring(7,4)
+                );
+
+            return saida;
+        }
+    }
+}
---
+++ agendasimplesmeupaiélindo/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace agenda_simples_t5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddContato_Click(object sender, EventArgs e)
+        {
+            Contato objetoContato = new Contato(txtNome.Text, txtSobrenome.Text, txtTelefone.Text, txtEmail.Text, txtIdade.Text);
+            lstContatos.Items.Add(objetoContato.ToString());
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTelefone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIdade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIdade_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
